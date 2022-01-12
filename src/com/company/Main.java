071cfc031884300547fe3b64dca5a7bd5e4222c5
erfan.adine.javadeveloper@gmail.com <erfan.adine.javadeveloper@gmail.com>
2022-01-12 18:59:48 +0330

    5th Main before mergeing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,12 @@ public class Main {
 	// write your code here
 	    
 	    //changes in Remote and feched in local repository
-	    
-	    public static void hello(){
-	    System.out.println("hello world");
-	    }
+
 
 	    //this is a test application
         System.out.println();
     }
+	    public static void hello(){
+	    System.out.println("hello world");
+	    }
 }
