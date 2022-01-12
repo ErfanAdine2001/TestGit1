dc85ce074db709bd9fed0905e1e506e6559ebc45
ErfanAdine2001 <89961037+ErfanAdine2001@users.noreply.github.com>
2022-01-12 18:40:33 +0330

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+	    
+	    //changes in Remote and feched in local repository
+	    
+	    public static void hello(){
+	    System.out.println("hello world");
+	    } 
     }
 }
